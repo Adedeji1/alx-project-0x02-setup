e896b23bafd97e7a6b22c2273dbee53100c2adb7
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-11-19 06:03:28 +0100

    addition of home and about navigation

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Link from "next/link";
 
 export default function Header () {
      return (
     <header style={{ padding: "20px", background: "#f5f5f5" }}>
+      <nav className="text-black">
+        <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+      </nav>
       <h1>My Next.js Project</h1>
     </header>
   );
